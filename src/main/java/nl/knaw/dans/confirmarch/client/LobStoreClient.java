47061a57aa0bd55e0e399cd977e934cefc159227
Jan van Mansum <janvanmansum@gmail.com>
2026-06-23 10:19:18 +0200

    Junie implementation

---
+++ src/main/java/nl/knaw/dans/confirmarch/client/LobStoreClient.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2025 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.confirmarch.client;
+
+import io.dropwizard.client.JerseyClientConfiguration;
+import lombok.Getter;
+import nl.knaw.dans.confirmarch.config.ServiceConfig;
+import nl.knaw.dans.lib.util.ClientProxyBuilder;
+import nl.knaw.dans.lobstore.client.invoker.ApiClient;
+import nl.knaw.dans.lobstore.client.invoker.ApiException;
+import nl.knaw.dans.lobstore.client.resources.DefaultApi;
+
+@Getter
+public class LobStoreClient {
+    private final DefaultApi api;
+
+    public LobStoreClient(ServiceConfig serviceConfig, JerseyClientConfiguration defaultHttpClient) {
+        api = new ClientProxyBuilder<ApiClient, DefaultApi>()
+            .apiClient(new ApiClient())
+            .defaultApiCtor(DefaultApi::new)
+            .basePath(serviceConfig.getUrl())
+            .httpClient(serviceConfig.getHttpClient() == null ? defaultHttpClient : serviceConfig.getHttpClient())
+            .build();
+    }
+
+    public boolean isLobPresent(String datastation, String hash) throws ApiException {
+        try {
+            api.getLocationByHash(datastation, hash);
+            return true;
+        }
+        catch (ApiException e) {
+            if (e.getCode() == 404) {
+                return false;
+            }
+            throw e;
+        }
+    }
+}
